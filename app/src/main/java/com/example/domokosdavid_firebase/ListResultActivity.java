20a0cf251683327db502b88348a0be1d128662e3
Domokos David <domokosdavid4@gmail.com>
2022-03-31 00:57:01 +0200

    egyedi ikon kész(plusz feladat)

--- app/src/main/java/com/example/domokosdavid_firebase/ListResultActivity.java
+++ app/src/main/java/com/example/domokosdavid_firebase/ListResultActivity.java
@@ -1,5 +1,6 @@
 package com.example.domokosdavid_firebase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,16 +9,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ListResultActivity extends AppCompatActivity {
 
     private TextView textViewOrszagok;
     private Button buttonVissza;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_result);
         init();
+
+
 
         buttonVissza.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +36,10 @@ public class ListResultActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
+
+
     public void init(){
         textViewOrszagok = findViewById(R.id.textViewOrszagok);
         buttonVissza = findViewById(R.id.buttonVissza);
