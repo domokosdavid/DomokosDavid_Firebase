0964152d34f26173b87207a3257dd9327eeb7a40
Domokos David <domokosdavid4@gmail.com>
2022-03-31 00:10:27 +0200

    adatfelvetel mukodik, input mezo validalva, toast messagek

--- app/src/main/java/com/example/domokosdavid_firebase/InsertActivity.java
+++ app/src/main/java/com/example/domokosdavid_firebase/InsertActivity.java
@@ -1,5 +1,6 @@
 package com.example.domokosdavid_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertActivity extends AppCompatActivity {
 
     private EditText editNev, editOrszag, editLakossag;
     private Button buttonUjFelvetel, buttonUjVissza;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,26 @@ public class InsertActivity extends AppCompatActivity {
         buttonUjFelvetel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String nev, orszag, lakossag;
+                nev = editNev.getText().toString();
+                orszag = editOrszag.getText().toString();
+                lakossag = editLakossag.getText().toString();
+                if (nev.isEmpty()){
+                    Toast.makeText(InsertActivity.this, "Hozzáadás sikertelen!", Toast.LENGTH_SHORT).show();
+                    editNev.setError("Nem lehet üres!");
+                }else if (orszag.isEmpty()){
+                    Toast.makeText(InsertActivity.this, "Hozzáadás sikertelen!", Toast.LENGTH_SHORT).show();
+                    editOrszag.setError("Nem lehet üres!");
+                } else if(lakossag.isEmpty()){
+                    Toast.makeText(InsertActivity.this, "Hozzáadás sikertelen!", Toast.LENGTH_SHORT).show();
+                    editLakossag.setError("Nem lehet üres!");
+                }else{
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    databaseReference = firebaseDatabase.getReference("városok");
+                    Varosok varosok = new Varosok(nev, orszag, lakossag);
+                    databaseReference.child(nev).setValue(varosok);
+                    Toast.makeText(InsertActivity.this, "Hozzáadás sikeres!", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -42,5 +73,6 @@ public class InsertActivity extends AppCompatActivity {
         editLakossag = findViewById(R.id.editLakossag);
         buttonUjFelvetel = findViewById(R.id.buttonUjFelvetel);
         buttonUjVissza = findViewById(R.id.buttonUjVissza);
+
     }
 }
---
+++ app/src/main/java/com/example/domokosdavid_firebase/Varosok.java
@@ -0,0 +1,24 @@
+package com.example.domokosdavid_firebase;
+
+public class Varosok {
+    private String nev, orszag, lakossag;
+
+    public Varosok(String nev, String orszag, String lakossag) {
+        this.nev = nev;
+        this.orszag = orszag;
+        this.lakossag = lakossag;
+    }
+    public Varosok(){};
+
+    public String getNev() {
+        return this.nev;
+    }
+
+    public String getOrszag() {
+        return this.orszag;
+    }
+
+    public String getLakossag() {
+        return this.lakossag;
+    }
+}
